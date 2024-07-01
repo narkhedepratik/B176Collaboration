788baf4a60bc622711fef121b00eede4e6a57dcc
narkhedepratik <pratiknarkhede039@gmail.com>
2024-07-01 18:15:51 +0530

    get api added

--- GitCollaborationDemo/src/main/java/com/cjc/main/controller/StudentController.java
+++ GitCollaborationDemo/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,5 +1,11 @@
 package com.cjc.main.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +23,13 @@ public class StudentController {
 		return student;
 	}
 	
+	@GetMapping("/students")
+	public ResponseEntity<List<Student>> exposeAllStudents()
+	{
+		return new ResponseEntity<List<Student>>
+		          (Arrays.asList(new Student(1,"sagar","pune"),
+		        		  new Student(2,"shubham","jalgoan"))
+				, HttpStatus.OK);
+		
+	}
 }
