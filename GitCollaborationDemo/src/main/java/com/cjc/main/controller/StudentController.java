315490cfb213cceb611c939c68639419299c252a
prajaktapawarpatil <pawarprajakta1404@gmail.com>
2024-07-01 18:05:22 +0530

    Git

--- GitCollaborationDemo/src/main/java/com/cjc/main/controller/StudentController.java
+++ GitCollaborationDemo/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,8 +1,20 @@
 package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.model.Student;
 
 @RestController
 public class StudentController {
 
+	
+	@PostMapping("/Student")
+	public Student postMethodName(@RequestBody Student student) {
+		System.out.println("This is Git Collaborators");
+		
+		return student;
+	}
+	
 }
